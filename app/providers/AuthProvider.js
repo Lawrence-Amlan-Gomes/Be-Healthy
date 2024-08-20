77e899ce15214446d88310c7ed8857a8df11e279
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2024-08-20 22:37:10 +0600

    auth context edited

--- app/providers/AuthProvider.js
+++ app/providers/AuthProvider.js
@@ -5,7 +5,7 @@ import {useState} from 'react';
 import { AuthContext } from '../contexts';
 
 export default function AuthProvider({children}) {
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(null);
 
     return(
         <AuthContext.Provider value={{auth, setAuth}}>
