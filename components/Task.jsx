eae3beaacc706beb32d1ccf3bbd970999c4c6bb4
Lawrence-Amlan-Gomes <amlangomes@gmail.com>
2024-09-16 02:00:53 +0600

    isBorder added

--- components/Task.jsx
+++ components/Task.jsx
@@ -2,18 +2,31 @@
 
 import { useTask } from "@/app/hooks/useTask";
 import { useTheme } from "@/app/hooks/useTheme";
-import { useState } from "react";
+import { Flamenco } from "next/font/google";
+import { useEffect, useState } from "react";
 
 export default function Task({ name, time, day, id, color }) {
   const { theme } = useTheme();
-  const { setTask, setClicked } = useTask();
+  const [isborder, setisborder] = useState(true)
+  const { task, setTask, setClicked } = useTask();
+  useEffect(()=>{
+    if(task.name == name && task.day == day){
+      if(task.time == time && task.id == id){
+        setisborder(true)
+      }else{
+        setisborder(false)
+      }
+    }else{
+      setisborder(false)
+    }
+  },[day, id, name, task.day, task.id, task.name, task.time, time])
   return (
     <div
       onClick={() =>{
         setTask({ name: name, time: time, day: day, id:id })
         setClicked(true)
       } }
-      className={`cursor-pointer w-[98%] h-[30px] text-white float-left ${color} rounded-lg m-[1%] text-start p-1 font-normal text-[14px] overflow-hidden scrollbar-none`}
+      className={`${isborder ? theme ? "bg-[#000000] text-white" : "bg-[#ffffff] text-black" : `${color} text-white`} cursor-pointer w-[98%] h-[30px] float-left rounded-lg m-[1%] text-start p-1 font-normal text-[14px] overflow-hidden scrollbar-none`}
     >
       <div className="w-[55%] h-full float-left px-1 mr-[5%] overflow-hidden">{name}</div>
       <div className="w-[40%] h-full overflow-hidden text-[12px] mt-[2px]">{time}</div>
