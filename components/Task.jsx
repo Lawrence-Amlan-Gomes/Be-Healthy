afdad9c6afc6f175919942a5ab28bef2116c4ac3
Lawrence-Amlan-Gomes <amlangomes@gmail.com>
2024-09-19 02:02:08 +0600

    text time padding

--- components/Task.jsx
+++ components/Task.jsx
@@ -29,7 +29,7 @@ export default function Task({ name, time, day, id, color }) {
       className={`${isborder ? theme ? "bg-[#000000] text-white" : "bg-[#ffffff] text-black" : `${color} text-white`} cursor-pointer w-[98%] h-[30px] float-left rounded-lg m-[1%] text-start p-1 font-normal text-[14px] overflow-hidden scrollbar-none`}
     >
       <div className="w-[55%] h-full float-left px-1 mr-[5%] overflow-hidden">{name}</div>
-      <div className="w-[40%] h-full overflow-hidden text-[12px] mt-[2px]">{time}</div>
+      <div className="w-[40%] h-full overflow-hidden text-end pr-2 text-[12px] mt-[2px]">{time}</div>
     </div>
   );
 }
